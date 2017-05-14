4c032178d723a58b019e196ab898d8693bbb55aa
Anthony Cornetto <Anthony Cornetto>
2017-05-14 19:42:41 -0400

    push

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.use(express.static('public'));
 
-//#region Routes
+//#region Routes 
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
